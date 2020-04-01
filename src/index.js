59374a68ab0cfb925ae28c3c8665b08711e86754
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2020-04-01 11:46:26 +0200

    coherent export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 // core
-export { default as TimeEngine } from './core/TimeEngine';
-export { default as PriorityQueue } from './core/PriorityQueue';
-export { default as SchedulingQueue } from './core/SchedulingQueue';
+import _TimeEngine from './core/TimeEngine';
+import _PriorityQueue from './core/PriorityQueue';
+import _SchedulingQueue from './core/SchedulingQueue';
 
 // masters
-export { default as PlayControl } from './masters/PlayControl';
-export { default as Transport } from './masters/Transport';
-export { default as Scheduler } from './masters/Scheduler';
-export { default as SimpleScheduler } from './masters/SimpleScheduler';
+import _PlayControl from './masters/PlayControl';
+import _Transport from './masters/Transport';
+import _Scheduler from './masters/Scheduler';
+import _SimpleScheduler from './masters/SimpleScheduler';
+
+// explicit default export
+export default {
+  TimeEngine: _TimeEngine,
+  PriorityQueue: _PriorityQueue,
+  SchedulingQueue: _SchedulingQueue,
+  PlayControl: _PlayControl,
+  Transport: _Transport,
+  Scheduler: _Scheduler,
+  SimpleScheduler: _SimpleScheduler,
+};
+
+// allow specific imports
+export const TimeEngine = _TimeEngine;
+export const PriorityQueue = _PriorityQueue;
+export const SchedulingQueue = _SchedulingQueue;
+export const PlayControl = _PlayControl;
+export const Transport = _Transport;
+export const Scheduler = _Scheduler;
+export const SimpleScheduler = _SimpleScheduler;
